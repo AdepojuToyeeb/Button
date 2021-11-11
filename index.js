843fff80b54cff1180575c63b254aa62da94e163
toyeeb <adewale.adepoju14@gmail.com>
2021-11-11 23:37:28 +0100

    Modified Index.js file

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-<script>
 function myFunction() {
     document.getElementById("myNumber").stepUp(1);
 }
+
 function mySecondFunction() {
     document.getElementById("myNumber").stepDown(1); 
 
 }
-</script>
