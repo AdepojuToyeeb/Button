088e8055835023b718786fcc51d7d4ce105b0918
toyeeb <adewale.adepoju14@gmail.com>
2021-11-11 23:35:40 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+<script>
+function myFunction() {
+    document.getElementById("myNumber").stepUp(1);
+}
+function mySecondFunction() {
+    document.getElementById("myNumber").stepDown(1); 
+
+}
+</script>
